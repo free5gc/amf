afc51ac6ea0c675e273f77c0ac923b9bead4dbb7
Yan-Jie Lin <linyj.cs08g@nctu.edu.tw>
2020-03-25 16:31:55 +0800

    add allow cross origin header

--- OAM/api_registered_ue_context.go
+++ OAM/api_registered_ue_context.go
@@ -6,7 +6,15 @@ import (
 	"gofree5gc/src/amf/amf_handler/amf_message"
 )
 
+func setCorsHeader(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
+}
+
 func RegisteredUEContext(c *gin.Context) {
+	setCorsHeader(c)
 
 	req := http_wrapper.NewRequest(c.Request, nil)
 	if supi, exists := c.Params.Get("supi"); exists {
