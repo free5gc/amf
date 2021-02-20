666f5acbf36970a5d57a1747e4b95d2de26d5ef6
Merlin Chlosta <merlin.chlosta@rub.de>
2021-02-20 17:13:19 +0100

    Reject registration if old AMF transfer fails
    If UE registers with 5G-GUTI (it has a context), free5gc may search
    for a context on another AMF. If no context is found, it should
    signal that to UE to trigger re-registration. Otherwise the UE may use
    the (invalid) context again.

--- gmm/handler.go
+++ gmm/handler.go
@@ -738,6 +738,7 @@ func HandleRegistrationRequest(ue *context.AmfUe, anType models.AccessType, proc
 		err := consumer.SearchAmfCommunicationInstance(ue, amfSelf.NrfUri, models.NfType_AMF, models.NfType_AMF, &searchOpt)
 		if err != nil {
 			logger.GmmLog.Errorf("[GMM] %+v", err)
+			gmm_message.SendRegistrationReject(ue.RanUe[anType], nasMessage.Cause5GMMUEIdentityCannotBeDerivedByTheNetwork, "")
 			return err
 		}
 
@@ -751,6 +752,7 @@ func HandleRegistrationRequest(ue *context.AmfUe, anType models.AccessType, proc
 			ue.SecurityContextAvailable = false // need to start authentication procedure later
 		} else if err != nil {
 			logger.GmmLog.Errorf("UE Context Transfer Request Error[%+v]", err)
+			gmm_message.SendRegistrationReject(ue.RanUe[anType], nasMessage.Cause5GMMUEIdentityCannotBeDerivedByTheNetwork, "")
 		} else {
 			ue.CopyDataFromUeContextModel(*ueContextTransferRspData.UeContext)
 		}
