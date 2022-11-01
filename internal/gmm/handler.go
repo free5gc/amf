965ac302a5f407773f656594490c3a74f07ae5a4
mariakagi <72726417+mariakagi@users.noreply.github.com>
2022-11-01 14:27:31 +0900

    SMF sends PDU Session Establishment Reject to UE when it fails to release the old session with duplicated PSI (#74)
    * SMF sends PDU Session Establishment Reject to UE when it fails to release the old session with duplicated PSI
    
    * unset duplicated PSI flag when error occurs
    
    * modify local release

--- internal/gmm/handler.go
+++ internal/gmm/handler.go
@@ -125,19 +125,14 @@ func transport5GSMMessage(ue *context.AmfUe, anType models.AccessType,
 				}
 				ue.GmmLog.Warningf("Duplicated PDU session ID[%d]", pduSessionID)
 				smContext.SetDuplicatedPduSessionID(true)
-				response, errResponse, _, err := consumer.SendUpdateSmContextRequest(smContext, updateData, nil, nil)
+				response, _, _, err := consumer.SendUpdateSmContextRequest(smContext, updateData, nil, nil)
 				if err != nil {
+					ue.GmmLog.Errorf("Failed to update smContext, local release SmContext[%d]", pduSessionID)
+					ue.SmContextList.Delete(pduSessionID)
 					return err
 				} else if response == nil {
-					err := fmt.Errorf("PDU Session ID[%d] can't be released in DUPLICATE_SESSION_ID case", pduSessionID)
-					ue.GmmLog.Errorln(err)
-					if errResponse != nil && errResponse.BinaryDataN1SmMessage != nil {
-						gmm_message.SendDLNASTransport(ue.RanUe[anType], nasMessage.PayloadContainerTypeN1SMInfo,
-							errResponse.BinaryDataN1SmMessage, pduSessionID, 0, nil, 0)
-					} else {
-						gmm_message.SendDLNASTransport(ue.RanUe[anType], nasMessage.PayloadContainerTypeN1SMInfo,
-							smMessage, pduSessionID, nasMessage.Cause5GMMPayloadWasNotForwarded, nil, 0)
-					}
+					ue.GmmLog.Errorf("Response to update smContext is nil, local release SmContext[%d]", pduSessionID)
+					ue.SmContextList.Delete(pduSessionID)
 				} else if response != nil {
 					smContext.SetUserLocation(ue.Location)
 					responseData := response.JsonData
