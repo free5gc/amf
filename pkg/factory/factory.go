febd4637e8c12ff2e6d9212a871dd4838d53f7f4
tim-ywliu <68043973+tim-ywliu@users.noreply.github.com>
2023-08-07 01:09:58 +0800

    Revert "changed all sd to lowercase (#101)"
    This reverts commit 694b964f90e77091899c40991acd752dbc52615d.

--- pkg/factory/factory.go
+++ pkg/factory/factory.go
@@ -7,7 +7,6 @@ package factory
 import (
 	"fmt"
 	"io/ioutil"
-	"strings"
 
 	"github.com/asaskevich/govalidator"
 	"gopkg.in/yaml.v2"
@@ -30,15 +29,6 @@ func InitConfigFactory(f string, cfg *Config) error {
 		logger.CfgLog.Infof("Read config from [%s]", f)
 		if yamlErr := yaml.Unmarshal(content, cfg); yamlErr != nil {
 			return fmt.Errorf("[Factory] %+v", yamlErr)
-		}
-	}
-
-	// change sd to lowercase
-	PlmnSupportList := cfg.Configuration.PlmnSupportList
-	for i := range PlmnSupportList {
-		SNssaiList := PlmnSupportList[i].SNssaiList
-		for j := range SNssaiList {
-			SNssaiList[j].Sd = strings.ToLower(SNssaiList[j].Sd)
 		}
 	}
 
