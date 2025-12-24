e6656d3f3af7b3055303fb79f77bca99030d0330
roundspring2003 <hwilly40159@gmail.com>
2025-12-24 15:57:09 +0800

    chore: fix

--- cmd/main.go
+++ cmd/main.go
@@ -13,7 +13,7 @@ import (
 	"github.com/free5gc/amf/internal/logger"
 	"github.com/free5gc/amf/pkg/factory"
 	"github.com/free5gc/amf/pkg/service"
-	logger_util "github.com/free5gc/util/logger"
+	logger_util "github.com/free5gc/util/logger
 	"github.com/free5gc/util/version"
 )
 
