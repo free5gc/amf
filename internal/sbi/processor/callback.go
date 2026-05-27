7340bfc896d3e393a74a226436220ddf0c5a845e
wiwi878 <a31625802@gmail.com>
2026-05-27 02:09:04 +0000

    fix: dereference anN2ApId after main merge

--- internal/sbi/processor/callback.go
+++ internal/sbi/processor/callback.go
@@ -322,15 +322,16 @@ func (p *Processor) N1MessageNotifyProcedure(n1MessageNotify models.N1MessageNot
 		return problemDetails
 	}
 
-	ranUe := ran.RanUeFindByRanUeNgapID(int64(*registrationCtxtContainer.AnN2ApId))
+	anN2ApId := *registrationCtxtContainer.AnN2ApId
+	ranUe := ran.RanUeFindByRanUeNgapID(int64(anN2ApId))
 	if ranUe == nil {
 		logger.CallbackLog.Warnf("RanUe Context[AnN2ApId:%d] not found in RAN[RanId: %+v]",
-			*registrationCtxtContainer.AnN2ApId, *registrationCtxtContainer.RanNodeId)
+			anN2ApId, *registrationCtxtContainer.RanNodeId)
 
 		problemDetails := &models.ProblemDetails{
 			Status: http.StatusNotFound,
 			Cause:  "CONTEXT_NOT_FOUND",
-			Detail: fmt.Sprintf("RanUe Context[AnN2ApId:%d] not found", *registrationCtxtContainer.AnN2ApId),
+			Detail: fmt.Sprintf("RanUe Context[AnN2ApId:%d] not found", anN2ApId),
 		}
 		return problemDetails
 	}
@@ -355,9 +356,9 @@ func (p *Processor) N1MessageNotifyProcedure(n1MessageNotify models.N1MessageNot
 
 		amfUe.CopyDataFromUeContextModel(ueContext)
 
-		currentRanUe := ran.RanUeFindByRanUeNgapID(int64(registrationCtxtContainer.AnN2ApId))
+		currentRanUe := ran.RanUeFindByRanUeNgapID(int64(anN2ApId))
 		if currentRanUe == nil {
-			logger.CallbackLog.Warnf("RanUe not found for AnN2ApId: %d", registrationCtxtContainer.AnN2ApId)
+			logger.CallbackLog.Warnf("RanUe not found for AnN2ApId: %d", anN2ApId)
 			return
 		}
 
