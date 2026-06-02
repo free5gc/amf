8050843c905a0a9fc09eff7a98cbc3dc746a3241
Sai Asish Y <say.apm35@gmail.com>
2026-06-02 12:26:09 -0700

    fix(sbi/consumer): set problem detail before returning on missing JsonData
    Signed-off-by: Sai Asish Y <say.apm35@gmail.com>

--- internal/sbi/consumer/amf_service.go
+++ internal/sbi/consumer/amf_service.go
@@ -291,6 +291,7 @@ func (s *namfService) UEContextTransferRequest(
 		ueContextTransferRspData = res.UeContextTransferResponse200.JsonData
 		if ueContextTransferRspData == nil {
 			logger.ConsumerLog.Warnln("UeContextTransfer 200 response missing JsonData")
+			problemDetails = openapi.ProblemDetailsSystemFailure("invalid UE context transfer response: missing response data")
 			err = openapi.ReportError("UeContextTransfer 200 missing JsonData")
 			return ueContextTransferRspData, problemDetails, err
 		}
