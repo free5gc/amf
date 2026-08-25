82911aba7d5868ac269d83427b49e06800c88e55
carlhus <carl.hu.cs14@nycu.edu.tw>
2026-08-25 17:50:13 +0000

    style: organize AMF context imports

--- internal/context/context.go
+++ internal/context/context.go
@@ -11,6 +11,9 @@ import (
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+
 	"github.com/free5gc/amf/internal/logger"
 	"github.com/free5gc/amf/pkg/factory"
 	"github.com/free5gc/nas/ie"
@@ -19,8 +22,6 @@ import (
 	"github.com/free5gc/openapi/models"
 	"github.com/free5gc/openapi/oauth"
 	"github.com/free5gc/util/idgenerator"
-	"github.com/google/uuid"
-	"github.com/pkg/errors"
 )
 
 var (
